df7d8e4adf0b8e2fb488ad32b13ef45cfa8a29e9
Patrik Marin <dev@patrikmarin.fi>
2024-02-05 23:07:49 +0200

    Time reading correctly with interrupts

--- CO22PI.ino
+++ CO22PI.ino
@@ -1,4 +1,6 @@
-#include "sdcard.h"
+#include <SdFat.h>
+#include <SPI.h>
+#include "pins.h"
 
 #define INIT_LED            \
 	{                       \
@@ -13,73 +15,100 @@
 		digitalWrite(2, HIGH); \
 	}
 // ------------------------
+
+volatile int last_miso = 0;
+volatile int last_mosi = 0;
+
+volatile bool should_read = false;
+volatile bool reading = false;
+
 void setup()
 {
 	Serial.begin(9600);
 	INIT_LED;
 	LED_ON;
 
-	Serial.println("Starting...");
+	Serial.println("");
+	Serial.println("[INFO] Starting...");
 
 	while (!Serial)
 	{
 		yield();
 	}
 
-	sdcard.attachInterrupts();
-	sdcard.giveControl();
-	delay(2000);
+	delay(1000);
 
-	Serial.println("Started!");
+	pinMode(MISO_PIN, INPUT);
+	pinMode(MOSI_PIN, INPUT);
+	pinMode(SCLK_PIN, INPUT);
+	pinMode(SD_CS_PIN, INPUT);
+
+	attachInterrupt(
+		digitalPinToInterrupt(MISO_PIN), []()
+		{ 
+			if (reading) return;
+			last_miso = millis();
+			Serial.println("MISO " + String(millis())); },
+		CHANGE);
+
+	attachInterrupt(
+		digitalPinToInterrupt(MOSI_PIN), []()
+		{
+			if (reading) return;
+			last_mosi = millis();
+			should_read = true;
+			Serial.println("MOSI " + String(millis())); },
+		CHANGE);
+
+	Serial.println("[INFO] Started!");
+
 	LED_OFF;
 }
 
-void loop()
+#define SPI_CLOCK SD_SCK_HZ(500000)
+#define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_HALF_SPEED)
+
+char *CSV_EXT = ".csv";
+bool DEBUG_ACTIVE = false;
+
+void loop(void)
 {
-
-	SdFat sd;
-	char fname[64], line[64];
-
-	static bool should_read = false;
-
 	if (should_read)
 	{
-		sdcard.takeControl();
-		while (!sdcard.initialise(&sd))
+		if (millis() - last_mosi > 5000)
 		{
-			delay(100);
-		}
-
-		Serial.println("Trying to read data");
-
-		if (sdcard.getCsvFile(&sd, fname) && sdcard.getFirstLine(&sd, fname, line))
-		{
-			Serial.print(line);
-			delay(200);
-			sdcard.deleteFile(&sd, fname);
 			should_read = false;
-			sd.end();
-			sdcard.giveControl();
-			delay(1000);
+			reading = true;
+
+			Serial.println("[INFO] Trying to read " + String(millis()));
+
+			pinMode(MISO_PIN, SPECIAL);
+			pinMode(MOSI_PIN, SPECIAL);
+			pinMode(SCLK_PIN, SPECIAL);
+			pinMode(SD_CS_PIN, OUTPUT);
+
+			SdFs sd;
+			if (sd.begin(SD_CONFIG))
+			{
+				sd.ls();
+				sd.end();
+			}
+			else
+			{
+				Serial.println("[ERR] Failed to start SD card");
+			}
+
+			pinMode(MISO_PIN, INPUT);
+			pinMode(MOSI_PIN, INPUT);
+			pinMode(SCLK_PIN, INPUT);
+			pinMode(SD_CS_PIN, INPUT);
+
+			reading = false;
+			should_read = false;
 		}
-		else
-		{
-			Serial.println("Failed to find data");
-			sd.end();
-			sdcard.giveControl();
-			delay(100);
-		}
 	}
-	else if (millis() - sdcard.last_mosi < 500)
-	{
-		Serial.println("Detected write");
-		should_read = true;
-		delay(1000);
-	}
-	else
-	{
-		yield();
-	}
+
+	delay(1000);
 }
 
 void blink()
