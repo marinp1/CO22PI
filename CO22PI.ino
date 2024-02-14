adcdd56365c4df17e9c79f0a847d79d8406d3ff5
Patrik Marin <dev@patrikmarin.fi>
2024-02-14 11:25:08 +0200

    Refactor code and logging

--- CO22PI.ino
+++ CO22PI.ino
@@ -1,61 +1,66 @@
 #include <SdFat.h>
 #include <SPI.h>
+// Include pin related data
 #include "pins.h"
 
-#define INIT_LED            \
-	{                       \
-		pinMode(2, OUTPUT); \
-	}
-#define LED_ON                \
-	{                         \
-		digitalWrite(2, LOW); \
-	}
-#define LED_OFF                \
-	{                          \
-		digitalWrite(2, HIGH); \
-	}
-// ------------------------
-
+// Keep track of timestamps when pins have changed value
 volatile int last_miso = 0;
 volatile int last_mosi = 0;
-
+// Keeps track if MOSI_PIN has received new value after read
 volatile bool should_read = false;
+// Active when program reading data from SD card
 volatile bool reading = false;
 
-bool DEBUG_ACTIVE = false;
-
-void setup()
+enum LogLevel
 {
-	Serial.begin(9600);
-	INIT_LED;
-	LED_ON;
+	Debug,
+	Info,
+	Error,
+	None,
+};
 
-	Serial.println("");
-	if (DEBUG_ACTIVE)
+// Logs to show
+const LogLevel APP_LOG_LEVEL = LogLevel::Error;
+
+String logLevelToText(LogLevel level)
+{
+	switch (level)
 	{
-		Serial.println("[INFO] Starting...");
+	case LogLevel::Debug:
+		return "DEBUG";
+		break;
+	case LogLevel::Info:
+		return "INFO";
+		break;
+	case LogLevel::Error:
+		return "ERROR";
+		break;
+	default:
+		return "UNKNOWN";
+		break;
 	}
+}
 
-	while (!Serial)
+void log(LogLevel level, String line)
+{
+	if (APP_LOG_LEVEL == LogLevel::None)
 	{
-		yield();
+		return;
 	}
+	if (level >= APP_LOG_LEVEL)
+	{
+		Serial.println("[" + logLevelToText(level) + "] " + String(millis()) + " " + line);
+	}
+}
 
-	delay(1000);
-
-	pinMode(MISO_PIN, INPUT);
-	pinMode(MOSI_PIN, INPUT);
-	pinMode(SCLK_PIN, INPUT);
-	pinMode(SD_CS_PIN, INPUT);
-
+void attachInterrupts()
+{
 	attachInterrupt(
 		digitalPinToInterrupt(MISO_PIN), []()
 		{ 
 			if (reading) return;
 			last_miso = millis();
-			if (DEBUG_ACTIVE) {
-				Serial.println("MISO " + String(millis()));
-		 }; },
+			log(LogLevel::Debug, "Detected MISO change"); },
 		CHANGE);
 
 	attachInterrupt(
@@ -64,199 +69,164 @@ void setup()
 			if (reading) return;
 			last_mosi = millis();
 			should_read = true;
-			if (DEBUG_ACTIVE) {
-				Serial.println("MOSI " + String(millis()));
-		 }; },
+			log(LogLevel::Debug, "Detected MOSI change"); },
 		CHANGE);
-
-	if (DEBUG_ACTIVE)
-	{
-		Serial.println("[INFO] Started!");
-	}
-
-	LED_OFF;
 }
 
-#define SPI_CLOCK SD_SCK_HZ(500000)
-#define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_HALF_SPEED)
+void setup()
+{
+	log(LogLevel::Info, "Starting...");
+	INIT_LED;
+	LED_ON;
 
+	Serial.begin(9600);
+	Serial.println("");
+
+	while (!Serial)
+	{
+		yield();
+	}
+
+	delay(1000);
+	GIVE_SPI_CONTROL;	// Make sure that SD card is in INPUT mode
+	attachInterrupts(); // Listen to MOSI and MISO changes
+
+	LED_OFF;
+	log(LogLevel::Info, "Started!");
+}
+
+/* Return name of last modified CSV file.  */
 String getCsvFile(SdFs *sd)
 {
-
 	File32 file, dir;
-	char fname[64];
-
-	uint16 latestdate = 0, latesttime = 0;
-	uint16 moddate, modtime;
-
-	String latest_fname = "";
 
 	if (!dir.open("/"))
 	{
-		if (DEBUG_ACTIVE)
-		{
-			Serial.println("[ERR] Failed to open root directory!");
-		}
+
+		log(LogLevel::Error, "Failed to open root directory!");
+		return "";
 	}
-	else
+
+	// Variables to keep track
+	char fname[64];
+	uint16 latestdate = 0, latesttime = 0;
+	uint16 moddate, modtime;
+
+	// Filename to return
+	String latest_fname = "";
+
+	while (file.openNext(&dir, O_READ))
 	{
-		while (file.openNext(&dir, O_READ))
+		file.clearWriteError();
+		file.clearError();
+		if (!file.isHidden() && file.isFile())
 		{
-			file.clearWriteError();
-			file.clearError();
-			if (!file.isHidden() && file.isFile())
+			file.getName(fname, sizeof(fname));
+			file.getModifyDateTime(&moddate, &modtime);
+			if (String(fname).endsWith(".csv") && moddate > latestdate && modtime > latesttime)
 			{
-				file.getName(fname, sizeof(fname));
-				file.getModifyDateTime(&moddate, &modtime);
-				if (String(fname).endsWith(".csv"))
-				{
-					if (moddate > latestdate && modtime > latesttime)
-					{
-						latest_fname = String(fname);
-					}
-				}
+
+				latest_fname = String(fname);
 			}
-			file.close();
 		}
+		file.close();
 	}
 
 	return latest_fname;
 }
 
+/* Return 2nd line of given file. */
 String readLastLineOfFile(SdFs *sd, String fname)
 {
-	char linetoread[64];
-	char line[64] = "";
-
+	// Open file in read only mode
 	FsFile file = sd->open(fname, O_RDONLY);
 	file.rewind();
+
+	String line;
+	char linetoread[64];
 
 	int i = 2;
 	while (i-- > 0)
 	{
-		int n = file.fgets(linetoread, 64);
+		int n = file.fgets(linetoread, 64); // Line length should always be <<< 64
 		if (n <= 0)
 		{
-			if (DEBUG_ACTIVE)
-			{
-				Serial.println("[WARN] Read failed");
-			}
+			log(LogLevel::Error, "Reading file failed (fgets failed)");
 			file.close();
 			return "";
 		}
 		if (linetoread[n - 1] != '\n' && n == (sizeof(linetoread) - 1))
 		{
-			if (DEBUG_ACTIVE)
-			{
-				Serial.println("[WARN] Line too long");
-			}
+			log(LogLevel::Error, "Reading file failed (line too long)");
 			file.close();
 			return "";
 		}
-		strcpy(line, linetoread);
+		line = String(linetoread);
 	}
 
 	file.close();
 
-	String lineData = String(line);
-	lineData.trim();
-
-	return lineData;
+	line.trim();
+	return line;
 }
 
+/* Try to delete given file. */
 String deleteFile(SdFs *sd, String fname)
 {
 	if (sd->remove(fname))
 	{
-		if (DEBUG_ACTIVE)
-		{
-			Serial.println("[INFO] File deleted");
-		}
+		log(LogLevel::Info, "File deleted successfully");
 	}
 	else
 	{
-		if (DEBUG_ACTIVE)
-		{
-			Serial.println("[ERR] File deletion failed");
-		}
+		log(LogLevel::Error, "File deletion failed");
 	}
 }
 
 void loop(void)
 {
-	if (should_read)
+	if (should_read && (millis() - last_mosi > 5000))
 	{
-		if (millis() - last_mosi > 5000)
+		should_read = false; // Mark read as one even on failure
+		reading = true;
+
+		log(LogLevel::Info, "Starting read procedure");
+
+		SdFs sd;
+		if (sd.begin(SD_CONFIG))
 		{
-			should_read = false;
-			reading = true;
-
-			if (DEBUG_ACTIVE)
+			String fname = getCsvFile(&sd);
+			if (fname.length() == 0)
 			{
-				Serial.println("[INFO] Trying to read " + String(millis()));
-			}
 
-			SdFs sd;
-			if (sd.begin(SD_CONFIG))
-			{
-				String fname = getCsvFile(&sd);
-				if (fname.length() == 0)
-				{
-					if (DEBUG_ACTIVE)
-					{
-						Serial.println("[ERR] Failed to find file");
-					}
-				}
-				else
-				{
-					if (DEBUG_ACTIVE)
-					{
-
-						Serial.println("[INFO] Found: " + fname);
-					}
-					String line = readLastLineOfFile(&sd, fname);
-					if (line.length() == 0)
-					{
-						if (DEBUG_ACTIVE)
-						{
-
-							Serial.println("[ERR] Failed to read file");
-							deleteFile(&sd, fname);
-						}
-					}
-					else
-					{
-						Serial.println(line);
-						deleteFile(&sd, fname);
-					}
-				}
-				sd.end();
+				log(LogLevel::Error, "Failed to find suitable CSV file");
 			}
 			else
 			{
-				if (DEBUG_ACTIVE)
+				log(LogLevel::Info, "Found file with name: " + fname);
+				String line = readLastLineOfFile(&sd, fname);
+				if (line.length() == 0)
 				{
-					Serial.println("[ERR] Failed to start SD card");
+
+					log(LogLevel::Error, "Failed to read file");
 				}
+				else
+				{
+					Serial.println(line);
+				}
+				deleteFile(&sd, fname);
 			}
-
-			pinMode(MISO_PIN, INPUT);
-			pinMode(MOSI_PIN, INPUT);
-			pinMode(SCLK_PIN, INPUT);
-			pinMode(SD_CS_PIN, INPUT);
-
-			reading = false;
-			should_read = false;
+			sd.end();
 		}
+		else
+		{
+
+			log(LogLevel::Error, "Failed to start SD card");
+		}
+
+		GIVE_SPI_CONTROL; // Set SPI as INPUT
+		reading = false;
 	}
 
-	delay(1000);
-}
-
-void blink()
-{
-	LED_ON;
-	delay(50);
-	LED_OFF;
-	delay(50);
+	delay(1000); // Wait before trying to read again
 }
--- pins.h
+++ pins.h
@@ -1,10 +1,36 @@
 #ifndef _SD_H_
 #define _SD_H_
 
+#define LED_PIN 2
 #define SD_CS_PIN 4
 #define MISO_PIN 12 // RISING received when reading data
 #define MOSI_PIN 13 // RISING received when writing data
 #define SCLK_PIN 14
 #define CS_SENSE 5
+
+#define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_HALF_SPEED)
+
+#define INIT_LED                  \
+    {                             \
+        pinMode(LED_PIN, OUTPUT); \
+    }
+
+#define LED_ON                      \
+    {                               \
+        digitalWrite(LED_PIN, LOW); \
+    }
+
+#define LED_OFF                      \
+    {                                \
+        digitalWrite(LED_PIN, HIGH); \
+    }
+
+#define GIVE_SPI_CONTROL           \
+    {                              \
+        pinMode(MISO_PIN, INPUT);  \
+        pinMode(MOSI_PIN, INPUT);  \
+        pinMode(SCLK_PIN, INPUT);  \
+        pinMode(SD_CS_PIN, INPUT); \
+    }
 
 #endif
